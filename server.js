f6ae78913ced40adc9789b5329e6852476954f6a
Rowan <48519289+scar055@users.noreply.github.com>
2026-02-05 14:18:28 +0100

    add custom data to server

--- server.js
+++ server.js
@@ -41,9 +41,22 @@ app.use(express.urlencoded({extended: true}));
 // Om Views weer te geven, heb je Routes nodig
 // Maak een GET route voor de index (meestal doe je dit in de root, als /)
 // In je visitekaartje was dit waarschijnlijk index.html
+const customData = JSON.parse(personResponseJSON.data.custom);
+
 app.get("/", async function (request, response) {
   // Render index.liquid uit de Views map en geef de opgehaalde data mee, in een variabele genaamd person
-  response.render("index.liquid", {person: personResponseJSON.data});
+  response.render("index.liquid", {
+    person: personResponseJSON.data,
+    custom: customData,
+  });
+});
+
+app.get("/oefenen", async function (request, response) {
+  // Render practice.liquid uit de Views map en geef de opgehaalde data mee, in een variabele genaamd person
+  response.render("practice.liquid", {
+    person: personResponseJSON.data,
+    custom: customData,
+  });
 });
 
 // Had je meer pagina's in je oude visitekaartje? Zoals een contact.html?
